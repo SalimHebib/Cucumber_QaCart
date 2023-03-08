82ebbe4e306d9c4500c9aa0e9d210694b425154b
salim <salamo.hebib@gmail.com>
2023-03-08 14:09:32 -0500

    Report Upgraded

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions",
+@CucumberOptions(
+        monochrome = true,
+        features = "src/test/java/features",
+        glue = "stepDefinitions",
         plugin = {"pretty", "html:target/Reports/Reports.html", "json:target/Reports/Reports.json"})
 
 
